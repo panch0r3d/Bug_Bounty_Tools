671f0dc6506304e984a87672a3af62dfd329aef5
panch0r3d <47708974+panch0r3d@users.noreply.github.com>
2020-11-10 17:49:16 -0600

    Update runSSRFchecks.sh

--- DNS_SSRF/runSSRFchecks.sh
+++ DNS_SSRF/runSSRFchecks.sh
@@ -4,6 +4,7 @@ input=`echo "$1"`
 output=`echo "$2"`
 echo '[*] Start getSSRFResponses.py'
 pingaddr=`curl -IL http://pingb.in | grep -i "Location: " | sed 's/Location: //g' | cut -b 2-999999 | sed 's/\r//g'`
+echo "http://pingb.in/${pingaddr}"
 python ./getSSRFResponses.py ${input} ${pingaddr}.REPLACE.${pingaddr}.ns.pingb.in > ${output}
 echo "" >> ${output}
 echo "" >> ${output}
